053df68688ebea7422d45e009909f420aacbe2ee
rizkisadewa <rizkykysadewa@gmail.com>
2019-09-24 00:12:29 +0800

    Add Inline Styles in React : we declare the object to the const variable, then set it to the inline style by calling the const object that we created

---
+++ React/Add Inline Styles in React.js
@@ -0,0 +1,39 @@
+
+const styles = {
+  color: "purple",
+  fontSize : 40,
+  border : "2px solid purple"
+};
+// change code above this line
+class Colorful extends React.Component {
+  render() {
+    // change code below this line
+    return (
+      <div style={{color: styles.color, fontSize: styles.fontSize, border : styles.border}}>Style Me!</div>
+    );
+    // change code above this line
+  }
+};
+
+
+/*
+You may have noticed in the last challenge that there were several other syntax differences from HTML inline styles in addition to the style attribute set to a JavaScript object.
+First, the names of certain CSS style properties use camel case. For example, the last challenge set the size of the font with fontSize instead of font-size.
+Hyphenated words like font-size are invalid syntax for JavaScript object properties, so React uses camel case.
+As a rule, any hyphenated style properties are written using camel case in JSX.
+
+All property value length units (like height, width, and fontSize) are assumed to be in px unless otherwise specified.
+If you want to use em, for example, you wrap the value and the units in quotes, like {fontSize: "4em"}.
+Other than the length values that default to px, all other property values should be wrapped in quotes.
+
+If you have a large set of styles, you can assign a style object to a constant to keep your code organized.
+Uncomment the styles constant and declare an object with three style properties and their values.
+Give the div a color of "purple", a font-size of 40, and a border of "2px solid purple". Then set the style attribute equal to the styles constant.
+
+1. The styles variable should be an object with three properties.
+2. The styles variable should have a color property set to a value of purple.
+3. The styles variable should have a fontSize property set to a value of 40.
+4. The styles variable should have a border property set to a value of 2px solid purple.
+5. The component should render a div element.
+6. The div element should have its styles defined by the styles object.
+*/
