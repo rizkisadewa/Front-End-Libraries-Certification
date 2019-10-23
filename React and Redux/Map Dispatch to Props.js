cedf89d319c5a08df4fce36017e50aa3e090d4f2
rizkisadewa <rizkykysadewa@gmail.com>
2019-10-23 23:31:03 +0800

    Map Distpatch to Props: dispatching to props

---
+++ React and Redux/Map Dispatch to Props.js
@@ -0,0 +1,45 @@
+const addMessage = (message) => {
+  return {
+    type: 'ADD',
+    message: message
+  }
+};
+
+// change code below this line
+const mapDispatchToProps = (dispatch) => {
+  return{
+      submitNewMessage: function(message){
+      dispatch(addMessage(message));
+    }
+  }
+}
+
+/*
+
+
+The mapDispatchToProps() function is used to provide specific action creators to your React components so they can dispatch actions against the Redux store.
+It's similar in structure to the mapStateToProps() function you wrote in the last challenge.
+It returns an object that maps dispatch actions to property names, which become component props.
+However, instead of returning a piece of state, each property returns a function that calls dispatch with an action creator and any relevant action data.
+You have access to this dispatch because it's passed in to mapDispatchToProps() as a parameter when you define the function, just like you passed state to mapStateToProps().
+Behind the scenes, React Redux is using Redux's store.dispatch() to conduct these dispatches with mapDispatchToProps().
+This is similar to how it uses store.subscribe() for components that are mapped to state.
+
+For example, you have a loginUser() action creator that takes a username as an action payload.
+The object returned from mapDispatchToProps() for this action creator would look something like:
+
+{
+  submitLoginUser: function(username) {
+    dispatch(loginUser(username));
+  }
+}
+
+The code editor provides an action creator called addMessage(). Write the function mapDispatchToProps() that takes dispatch as an argument,
+ then returns an object. The object should have a property submitNewMessage set to the dispatch function,
+ which takes a parameter for the new message to add when it dispatches addMessage().
+
+1. addMessage should return an object with keys type and message.
+2. mapDispatchToProps should be a function.
+3. mapDispatchToProps should return an object.
+4. Dispatching addMessage with submitNewMessage from mapDispatchToProps should return a message to the dispatch function.
+*/
